90aac65db371f96e8609407bdef75eea391cf8a3
Kamola15-95 <136570298+Kamola15-95@users.noreply.github.com>
2023-09-06 22:17:54 +0500

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from database.database import DataBase
-bot = Bot('6037875681:AAFTM0bUFC_Y4wMQyA0SaLw_OSw-ZXjgpKw', parse_mode='HTML')
+bot = Bot('', parse_mode='HTML')
 db = DataBase()
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
